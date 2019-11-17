0ba2c625319da8bde5ad065a27f3a4033ab428bd
yaoli86 <55010269+yaoli86@users.noreply.github.com>
2019-11-17 21:42:46 +0800

    test emoji

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def build_PhotoInfo(image_gray, image_rgb, annotated_rgb, crop_faces):
 
 def likeornot(emoji):
     print(emoji['happy'])
-    Positive = float(emoji['happy']) + float(emoji['surprise']) + float(emoji['neutral'])
+    Positive = float(emoji['happy'])*10 + float(emoji['surprise']) + float(emoji['neutral'])
     Negative = float(emoji['angry']) + float(emoji['fear']) + float(emoji['sad'])
 
 
